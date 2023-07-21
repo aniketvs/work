babfd3af17030004771a4d208b767e923950071f
aniketvs <sharmavinod8454@gmail.com>
2023-07-21 17:22:29 +0530

    url

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,4 @@
 const mongoose=require('mongoose');
-const url = "mongodb+srv://Aniketvs:Aniketsharma%402111@cluster0.y4m5vhn.mongodb.net/portfolio?retryWrites=true&w=majority" // put your connection string
 mongoose.connect(url,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
